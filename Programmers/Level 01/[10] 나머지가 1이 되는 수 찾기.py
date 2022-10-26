c87c4f2b59b22e17cff20c63a7588c80d83c576a
김지환 <91196025+Kimjipang@users.noreply.github.com>
2022-10-26 21:14:59 +0900

    Update [10] 나머지가 1이 되는 수 찾기.py

--- Programmers/Level 01/[10] 나머지가 1이 되는 수 찾기.py
+++ Programmers/Level 01/[10] 나머지가 1이 되는 수 찾기.py
@@ -1,3 +1,5 @@
+======== 나의 풀이 ========
+
 def solution(n):
     answer = 0
     for i in range(2, n):
@@ -5,3 +7,12 @@ def solution(n):
             answer = i 
             break
     return answer
+
+
+======== 다른 풀이 ========
+
+def solution(n):    
+    
+    return [i for i in range(2, n) if(n % i == 1)][0]
+
+# 효율성 면에서는 위에가 나아보임! 아래는 무조건 for문을 다 돌려서 list를 만들기 때문에 시간복잡도가 같은 O(n)이긴 해도 다름.
