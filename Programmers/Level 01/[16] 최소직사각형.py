5f25ab43ca7a730c100a89c3029701090f51a10e
김지환 <91196025+Kimjipang@users.noreply.github.com>
2022-11-07 17:51:05 +0900

    Update [16] 최소직사각형.py

--- Programmers/Level 01/[16] 최소직사각형.py
+++ Programmers/Level 01/[16] 최소직사각형.py
@@ -24,4 +24,19 @@ def solution(sizes):
     return answer
   
   
-  
+#   ==========================================================
+#   이렇게 하면 일단 시간 복잡도가 O(n^2)이 나와서 좋은 코드는 아님.
+#   처음 풀었던 나의 풀이(위)와 다른 사람의 풀이(아래)는 결국에는 같은 행동을 하는 코드이지만 
+#   아래의 풀이가 효율성도 좋고 굳이 생각하지 않아도 되는 것에 대해서는 빼버리고 작성한 코드라 더 좋은 거 같다. 
+    
+    
+def solution(sizes):
+
+    row = col = 0
+    
+    for i, j in sizes:
+        if i < j:
+            i, j = j, i
+        row = max(row, i)
+        col = max(col, j)
+    return row * col
